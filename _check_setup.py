c786effa86f4d1b233cec0b6087e7538e7b9d659
Yiling Zhou <pastgift@gmail.com>
2020-11-29 15:45:50 +0800

    修复已知问题

--- _check_setup.py
+++ _check_setup.py
@@ -13,10 +13,16 @@ CHECK_INTERVAL = 3
 base_path   = os.path.dirname(os.path.abspath(__file__))
 config_path = os.path.join(base_path, './config.yaml')
 
+def disable_setup():
+    return yaml_resources.load_config(config_path).get('_DISABLE_SETUP')
+
 def is_installed():
     return yaml_resources.load_config(config_path).get('_IS_INSTALLED')
 
 def main():
+    if disable_setup():
+        sys.exit(0)
+
     while not is_installed():
         time.sleep(CHECK_INTERVAL)
         print('Waiting for setup.')
