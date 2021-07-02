07d681746861a4d3a5f47a637bbb2302cf1d8ab5
Yiling Zhou <pastgift@gmail.com>
2021-07-03 02:25:05 +0800

    test5

--- extra.py
+++ extra.py
@@ -1,12 +1,20 @@
 s = 'def load_default_certs(self, purpose=Purpose.SERVER_AUTH):'
 s2 = '''def load_default_certs(self, purpose=Purpose.SERVER_AUTH):
-        print('>>>>>', purpose)
-        print('>>>>>', type(purpose))
-        print('>>>>>', repr(purpose))
         print('>>>>>', _ASN1Object)
         print('>>>>>', type(_ASN1Object))
         print('>>>>>', repr(_ASN1Object))
+
+        print('>>>>>', purpose)
+        print('>>>>>', type(purpose))
+        print('>>>>>', repr(purpose))
         print('>>>>>', not isinstance(purpose, _ASN1Object))
+
+        print('>>>>>', Purpose.SERVER_AUTH)
+        print('>>>>>', type(Purpose.SERVER_AUTH))
+        print('>>>>>', repr(Purpose.SERVER_AUTH))
+        print('>>>>>', not isinstance(Purpose.SERVER_AUTH, _ASN1Object))
+
+        print('>>>>>', purpose == Purpose.SERVER_AUTH, purpose is Purpose.SERVER_AUTH)
 '''
 
 src = ''
