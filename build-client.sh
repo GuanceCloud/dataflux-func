05c7e4f69869ef9c4f2b477a32937dead76eabb5
Yiling Zhou <pastgift@gmail.com>
2022-05-05 18:07:30 +0800

    修复vue环境变量

--- build-client.sh
+++ build-client.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
-# run client DEV
+# build client
+VUE_APP_SERVER_BASE_URL=/
+echo "Server Base URL: ${VUE_APP_SERVER_BASE_URL}"
+
 cd client
 npm run build
--- run-client-dev.sh
+++ run-client-dev.sh
@@ -2,5 +2,7 @@
 set -e
 
 # run client DEV
+echo "Server Base URL: ${VUE_APP_SERVER_BASE_URL}"
+
 cd client
 npm run serve
