d582b13a099a3392ee2761d8ae82fc6c156af1a7
liwenjin <liwenjin@jiagouyun.com>
2022-05-06 18:03:53 +0800

    添加helm打包

---
+++ build-charts.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+# 此脚步用于打包Func charts 包
+# 生产地址为 https://pubrepo.guance.com/chartrepo/func  func-prod-chart
+# 测试地址为 https://registry.jiagouyun.com/chartrepo/func-test  func-test-chart
+# param VERSION  1.6.7
+# param IMAGETAG 镜像tag
+# param REPO helm 仓库地址
+
+VERSION=$1
+IMAGETAG=$2
+REPO=$3
+
+
+helm_info(){
+	helm repo ls
+}
+
+
+build_charts(){
+	sed "s,{{tag}},$IMAGETAG,g" charts/values.yaml > charts/func/values.yaml
+	sed "s,{{version}},$VERSION,g" charts/Chart.yaml > charts/func/Chart.yaml
+	helm package charts/func
+}
+
+push_charts(){
+	helm push func-$VERSION.tgz $(1)
+	rm -f func-$VERSION.tgz $REPO
+}
+
+helm_info()
+build_package()
+push_charts()
