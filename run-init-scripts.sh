ad373359bbb37bed0a3fda03df3e323addb5778d
Yiling Zhou <zyl@pastgift.com>
2023-04-23 21:41:36 +0800

    小调整

--- run-init-scripts.sh
+++ run-init-scripts.sh
@@ -7,7 +7,7 @@ mkdir -p ${pre_run_script_folder}
 
 # Copy old version
 old_pre_run_script_folder="`python _config.py RESOURCE_ROOT_PATH`/init-scripts"
-if [ -d ${old_pre_run_script_folder} ]; then
+if [ -d ${old_pre_run_script_folder} ] && [ "`ls -A ${old_pre_run_script_folder}`" != "" ]; then
   cp ${old_pre_run_script_folder}/* ${pre_run_script_folder}/
 fi
 
