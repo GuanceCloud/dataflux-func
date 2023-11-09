685d5ef22c34e82e0544d3f153dd225c64c5dfdf
Yiling Zhou <zyl@pastgift.com>
2023-11-09 18:07:16 +0800

    修复 DataKit, DataWay 行协议中 Tag Value 有关斜杠的转义错误的问题

--- worker/utils/extra_helpers/dataway.py
+++ worker/utils/extra_helpers/dataway.py
@@ -96,7 +96,7 @@ MIN_ALLOWED_NS_TIMESTAMP = 1000000000000000000
 
 ESCAPE_REPLACER           = r'\\\1'
 RE_NORMALIZE_SPACE        = re.compile('\s')
-RE_ESCAPE_TAG_KEY         = re.compile('([,= ])')
+RE_ESCAPE_TAG_KEY         = re.compile('([,= \\\\])')
 RE_ESCAPE_TAG_VALUE       = RE_ESCAPE_TAG_KEY
 RE_ESCAPE_FIELD_KEY       = RE_ESCAPE_TAG_KEY
 RE_ESCAPE_MEASUREMENT     = re.compile('([, ])')
